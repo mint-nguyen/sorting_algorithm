5cf3d8dc31a4fb677774ed0d01e662bfcc3325f4
Mint <mintnguyen.pa@gmail.com>
2023-05-22 10:36:55 -0700

    feat: bubble sort

---
+++ BubbleSort.js
@@ -0,0 +1,16 @@
+const BubbleSort = (arr) => {
+	for (let i = arr.length - 1; i >= 0; i--) {
+		for (let j = 0; j < i; j++) {
+			if (arr[j + 1] < arr[j]) {
+				let temp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+};
+
+const arr = [1, 64, 24, -5, -23];
+BubbleSort(arr);
+
+console.log(arr);
